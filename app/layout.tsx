a0a2372399701ade40da9409246ead2f998ade47
jerrycool123 <jerrycool71534@gmail.com>
2023-10-04 05:04:56 +0800

    chore: Google Site Verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +7,15 @@ import 'inter-ui/inter.css';
 import './globals.css';
 
 import { RootProviders } from '../contexts/RootProviders';
+
+export const metadata: Metadata = {
+  title: 'Discord OAuth 2.0 Playground',
+  description:
+    'The Discord OAuth 2.0 Playground helps you to play with OAuth 2.0 and make API requests.',
+  other: {
+    'google-site-verification': '2mKNWgX4OXy7j6Bmhf37edNJ1jXgXCbYObAfdMS2mqk',
+  },
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { Metadata } from 'next';
-
 import styles from './Home.module.css';
 
 import LeftPane from '../components/Panes/LeftPane';
 import RightPane from '../components/Panes/RightPane';
-
-export const metadata: Metadata = {
-  title: 'Discord OAuth 2.0 Playground',
-  description:
-    'The Discord OAuth 2.0 Playground helps you to play with OAuth 2.0 and make API requests.',
-};
 
 export default function Home() {
   return (
